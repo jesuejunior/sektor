f98cfa6672f2b259906a688536208e0a90107131
Gabriel Seixas <salesseixasgabriel@gmail.com>
2018-09-25 18:20:13 -0300

    Changed old location to be set on memory

--- sektor/main.py
+++ sektor/main.py
@@ -5,6 +5,12 @@ from math import radians, cos, sin, asin, sqrt
 from db import DB
 
 
+def clear_screen():
+    print(chr(27) + '[2j')
+    # print('\033c')
+    # print('\x1bc')
+
+
 class Sektor:
 
     def start():
@@ -14,14 +20,27 @@ class Sektor:
         gps_socket.connect()
         gps_socket.watch()
 
+        old_location = {'lat': 0, 'lon': 0}
+
         for new_data in gps_socket:
             if new_data:
+                print('getting new data...')
                 gps_stream.unpack(new_data)
-                location = Sektor.get_locations(gps_stream)
+                current_location = Sektor.get_locations(gps_stream)
 
-                saved_location = Sektor.save_location(location)
+                saved_location = Sektor.save_location(
+                    current_location,
+                    old_location
+                )
+
+                if saved_location:
+                    old_location['lat'] = current_location['lat']
+                    old_location['lon'] = current_location['lon']
+
+            clear_screen()
 
     def get_locations(gps_data):
+        print('ta chegando aqui')
         if hasattr(gps_data, 'time'):
             print(gps_data.time)
 
@@ -36,35 +55,40 @@ class Sektor:
 
     def save_location(gps_data, last_location):
         try:
-            distance = Sektor.check_distance(gps_data, last_location)
+            distance = Sektor.calc_distance(gps_data, last_location)
             saved = DB.save(
                 lat=gps_data.lat,
                 lon=gps_data.lon,
                 speed=gps_data.speed,
                 distance=distance,
-                oil=distance > 300
+                oil=Sektor.do_grease(gps_data)
             )
 
             return saved
         except Exception:
             return False
 
-    def do_grease(gps_data):
-        pass
-
-    def check_distance(current_location, ):
-        pass
+    def do_grease(gps_data, last_location):
+        gps_data.lat
 
     def turn_on_motor(report):
         pass
 
-    def calc_distance(lon1, lat1, lon2, lat2):
+    def get_last_location():
+        return 2
+
+    def calc_distance(coordinates1, coordinates2):
         """
         Calculate the great circle distance between two points
         on the earth (specified in decimal degrees)
         """
         # convert decimal degrees to radians
-        lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+        lon1, lat1, lon2, lat2 = map(radians, [
+            coordinates1['lon'],
+            coordinates1['lat'],
+            coordinates2['lon'],
+            coordinates2['lat']
+        ])
 
         # haversine formula
         dlon = lon2 - lon1
