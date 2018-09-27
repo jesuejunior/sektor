11de1a2d221dc1d0a0d1c83725090579ba21124c
Jesue Junior <jesuesousa@gmail.com>
2018-09-26 23:12:26 -0300

    Refactor

--- sektor/main.py
+++ sektor/main.py
@@ -1,10 +1,9 @@
 # encoding: utf-8
 import random
-from math import radians, cos, sin, asin, sqrt
 from datetime import datetime
 from gps3 import gps3
 
-from db import DB
+from position import Position
 
 
 def fake_data():
@@ -15,51 +14,6 @@ def fake_data():
             "speed": random.random(),
             "time": datetime.now(),
         }
-
-
-class Position:
-    def __init__(self, lat, lon, speed, time, distance=0, oil=False):
-        self.lat = lat
-        self.lon = lon
-        self.speed = int(speed)
-        self.distance = distance
-        self.time = time
-        self.oil = oil
-
-    @staticmethod
-    def calc_distance(coordinates1: "Position", coordinates2: "Position") -> float:
-        """
-        Calculate the great circle distance between two points
-        on the earth (specified in decimal degrees)
-        """
-        # convert decimal degrees to radians
-
-        lon1, lat1, lon2, lat2 = map(
-            radians,
-            [
-                float(coordinates1.lon),
-                float(coordinates1.lat),
-                float(coordinates2.lon),
-                float(coordinates2.lat),
-            ],
-        )
-        # haversine formula
-        dlon = lon2 - lon1
-        dlat = lat2 - lat1
-        a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
-        c = 2 * asin(sqrt(a))
-        r = 6371  # Radius of earth in kilometers. Use 3956 for miles
-        return c * r
-
-    def save(self, last_location):
-        try:
-            distance = Position.calc_distance(self, last_location)
-            self.distance = distance
-            self.oil = Sektor.do_grease(distance, self.speed)
-            return self if DB.save(**self.__dict__) else False
-        except Exception as ex:
-            print("Exception: ", ex)
-            return False
 
 
 class Sektor:
@@ -83,12 +37,16 @@ class Sektor:
                 if isinstance(data.TPV.get("lat"), float) and isinstance(
                     data.TPV.get("lon"), float
                 ):
+
                     position = Position(
                         data.TPV["lat"],
                         data.TPV["lon"],
                         data.TPV["speed"],
                         data.TPV["time"],
                     )
+                    distance = Position.calc_distance(position, last_location)
+                    oil = Sektor.do_grease(distance, position.speed)
+                    position.oil = oil
                     saved_location = position.save(last_position)
                     if saved_location:
                         last_position = Position(**saved_location.__dict__)
---
+++ sektor/position.py
@@ -0,0 +1,46 @@
+# encoding: utf-8
+from math import radians, cos, sin, asin, sqrt
+
+from db import DB
+
+
+class Position:
+    def __init__(self, lat, lon, speed, time, oil=False, distance=0):
+        self.lat = lat
+        self.lon = lon
+        self.speed = int(speed)
+        self.distance = distance
+        self.time = time
+        self.oil = oil
+
+    @staticmethod
+    def calc_distance(coordinates1: "Position", coordinates2: "Position") -> float:
+        """
+        Calculate the great circle distance between two points
+        on the earth (specified in decimal degrees)
+        """
+        # convert decimal degrees to radians
+
+        lon1, lat1, lon2, lat2 = map(
+            radians,
+            [
+                float(coordinates1.lon),
+                float(coordinates1.lat),
+                float(coordinates2.lon),
+                float(coordinates2.lat),
+            ],
+        )
+        # haversine formula
+        dlon = lon2 - lon1
+        dlat = lat2 - lat1
+        a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+        c = 2 * asin(sqrt(a))
+        r = 6371  # Radius of earth in kilometers. Use 3956 for miles
+        return c * r
+
+    def save(self, last_location):
+        try:
+            return self if DB.save(**self.__dict__) else False
+        except Exception as ex:
+            print("Exception: ", ex)
+            return False
