53a2465618163338cc9a814f5a117a09ec98e3d1
Jesue Junior <jesuesousa@gmail.com>
2018-10-10 16:02:13 -0300

    Turbine module initial

---
+++ sektor/turbine.py
@@ -0,0 +1,20 @@
+# encoding: utf-8
+import time
+from gpiozero import PWMOutputDevice
+
+
+class Turbine:
+    def wipe():
+        # PARAMS:  pin, *, active_high=True, initial_value=0, frequency=100, pin_factory=None
+        print("Starting cleaning time")
+        with PWMOutputDevice(23) as motor:
+            motor.on()
+            time.sleep(10)
+            motor.off()
+
+    def oil():
+        print("Starting oil on the chain")
+        with PWMOutputDevice(24) as motor:
+            motor.on()
+            time.sleep(15)
+            motor.off()
