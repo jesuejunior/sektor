ac8b8d3b5c318668346399fc7bd20dff30818afc
Jesue Junior <jesuesousa@gmail.com>
2018-10-14 01:26:39 -0300

    improviment startup process at boot

--- configure.sh
+++ configure.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 ip=$1
 
-rsync -rav pi@$ip:~/code/sektor /sektor
-
 conn="ssh pi@$ip"
 
-# copy files to configure ports
+${conn} 'sudo apt-get update && sudo apt-get install --yes python3-pip build-essential rsync && sudo pip3 install pipenv'
 
-# ....
+rsync -rav ../sektor pi@$ip:~/code/sektor
 
-${conn} 'sudo apt-get update'
-
-${conn} 'sudo apt-get install --yes python3-pip pipenv build-essentials'
-
-${conn} 'sudo cd /code && sudo pipenv install'
-
+${conn} 'cd /code/sektor; sudo pipenv --python /usr/bin/python3 install && sudo chmod +x /code/sektor/sektor/main.py'
 
 # Configuring port boudrate and starting gpsddeamon
-${conn} 'stty -F /dev/ttyS0 9600 && sudo gpsd /dev/ttyS0 -F /var/run/gpsd.socket'
+#${conn} 'stty -F /dev/ttyS0 9600 && sudo gpsd /dev/ttyS0 -F /var/run/gpsd.socket'
+
+
+${conn} 'sudo cp /code/sektor/files/sektor.service /lib/systemd/system/sektor.service && sudo systemctl daemon-reload && sudo systemctl enable sektor.service'
 
 
