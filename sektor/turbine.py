2a72314edde3d48ab57174b509c081f9eb9b5f0f
Jesue Junior <jesuesousa@gmail.com>
2018-11-01 11:27:44 -0300

    Fixing turbines

--- sektor/turbine.py
+++ sektor/turbine.py
@@ -7,14 +7,14 @@ class Turbine:
     def clean():
         # PARAMS:  pin, *, active_high=True, initial_value=0, frequency=100, pin_factory=None
         print("Starting cleaning time")
-        with PWMOutputDevice(23) as motor:
+        with PWMOutputDevice(04) as motor:
             motor.on()
             time.sleep(10)
             motor.off()
 
     def oil():
         print("Starting oil on the chain")
-        with PWMOutputDevice(24) as motor:
+        with PWMOutputDevice(17) as motor:
             motor.on()
-            time.sleep(15)
+            time.sleep(10)
             motor.off()
